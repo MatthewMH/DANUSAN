10eaa1fc52cd29fcafac387bed00de0191fce589
ainunzahida <79038435+ainunzahida@users.noreply.github.com>
2021-06-02 10:46:51 +0700

    update for profile

--- resources/views/toko.blade.php
+++ resources/views/toko.blade.php
@@ -13,8 +13,13 @@
 	</a>
 	<img src = "{{ URL::asset('images/tokoimages/Danusan.png') }}" class = "danusan">
 	<div class = "kanan-atas">
-		<img src = "{{ URL::asset('images/tokoimages/Vector1.png') }}" class = "orang-dalem">
-		<img src = "{{ URL::asset('images/tokoimages/Vector2.png') }}" class = "lingkar-luar">
+		@if(Auth()->user()->profil == NULL)
+			<img src = "{{ URL::asset('images/tokoimages/Vector1.png') }}" class = "orang-dalem">
+			<img src = "{{ URL::asset('images/tokoimages/Vector2.png') }}" class = "lingkar-luar">
+		@else
+			<img src = "{{ URL::asset('profile/'.Auth()->user()->username.'/'.Auth()->user()->profil) }}" class = "profil">
+			<img src = "{{ URL::asset('images/tokoimages/Vector2.png') }}" class = "lingkar-luar">
+		@endif
 		<span class = 'username'> {{ Auth()->user()->username }} </span>
 	</div>
 	<div class = "kotak-nama">
