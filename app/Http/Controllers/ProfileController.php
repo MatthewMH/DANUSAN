8ff330f8933dfe19665075d10039d4022b40e7a6
MatthewMH <63547189+MatthewMH@users.noreply.github.com>
2021-06-10 09:42:46 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\orangdanusan;
 use Auth;
+use Validator;
 
 class ProfileController extends Controller
 {
@@ -15,14 +16,39 @@ class ProfileController extends Controller
     }
     public function edit(Request $request)
     {
-    	$user = orangdanusan::find(Auth()->user()->id);
+        //dd($request->fotoprofil->extension());
+        $imageExtensions = ['jpg', 'jpeg', 'png'];
+        $validator = Validator::make($request->all(), [
+            'profil' => 'max:5120', //5MB 
+        ]);
+        if($validator->fails())
+        {
+            return redirect()->to('/editprofile/exception2/'.Auth()->user()->id);
+        }
         if($request->fotoprofil == NULL)
         {
-            return redirect()->to('/editprofile/'.Auth()->user()->id);
+            return redirect()->to('/editprofile/exception3/'.Auth()->user()->id);
         }
+        if (!in_array($request->fotoprofil->extension(), $imageExtensions)) 
+        {
+            return redirect()->to('/editprofile/exception1/'.Auth()->user()->id);
+        }
+    	$user = orangdanusan::find(Auth()->user()->id);
     	$user->profil = Auth()->user()->username.'.'.$request->fotoprofil->extension();
     	$request->fotoprofil->move(public_path('profile/'.$user->username), $user->profil);
    		$user->save();
    		return redirect()->to('/editprofile/'.Auth()->user()->id);
+    }
+    public function view_exception1()
+    {
+        return view('profile_exception1');
+    }
+    public function view_exception2()
+    {
+        return view('profile_exception2');
+    }
+    public function view_exception3()
+    {
+        return view('profile_exception3');
     }
 }
