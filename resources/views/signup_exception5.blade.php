b4106902150e8fc128a656c4a983e673ab38bef1
MatthewMH <63547189+MatthewMH@users.noreply.github.com>
2021-06-13 15:53:57 +0700

    Add files via upload

---
+++ resources/views/signup_exception5.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Lets+Espresso&display=swap" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+	<link href="{{ URL::asset('css/SignUpLogin_exceptioncss/main.css') }}" rel="stylesheet" />
+	<title>Sign Up</title>
+</head>
+<body>
+	<div class="v0_3">
+		<a href = "{{ url('/') }}">
+			<img src = "{{ URL::asset('images/signup_exceptionimages/arrow.png') }}" class = "arrow">
+		</a>
+		<img src = "{{ URL::asset('images/signup_exceptionimages/Danusan.png') }}" class = "danusan">
+		<img src = "{{ URL::asset('images/signup_exceptionimages/vektor sign up page.png') }}" class = "gambarkiri">
+		<img src = "{{ URL::asset('images/signup_exceptionimages/Rectangle 26.png') }}" class = "kotak">
+		<form action = "/signup">
+			<div class="v414_26">
+			<span class="v409_90">Sign Up</span>
+				<div class="v409_91">
+					<span class = "tulisan">Username yang dimasukkan terlalu panjang!</span>
+				</div>
+			</div>
+			<input type = "submit" class = "kotaksignup" value = "Back">
+		</form>
+		<img src = "{{ URL::asset('images/signup_exceptionimages/v178_142.png') }}" class = "gambarkanan">
+</body>
+</html>
