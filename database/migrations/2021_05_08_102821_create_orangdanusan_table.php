0b579441c3f1cc517b873b99b6bdc5c9b9ae86d8
MatthewMH <63547189+MatthewMH@users.noreply.github.com>
2021-06-13 15:50:17 +0700

    Update 2021_05_08_102821_create_orangdanusan_table.php

--- database/migrations/2021_05_08_102821_create_orangdanusan_table.php
+++ database/migrations/2021_05_08_102821_create_orangdanusan_table.php
@@ -16,11 +16,11 @@ class CreateOrangdanusanTable extends Migration
         Schema::connection('pgsql')->create('orangdanusan', function (Blueprint $table) {
 
             $table->id();
-            $table->string('email');
-            $table->string('username');
-            $table->string('kontak');
-            $table->string('password');
-            $table->string('profil')->nullable();
+            $table->string('email', 255);
+            $table->string('username', 255);
+            $table->string('kontak', 13);
+            $table->string('password', 255);
+            $table->string('profil', 255)->nullable();
         });
     }
 
